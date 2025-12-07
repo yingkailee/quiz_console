5b49c36b084091db9ec11ddfff6eeb29aae3ead6
yingkailee <yingkailee0@gmail.com>
2025-12-07 15:57:56 -0600

    convert options to array

--- Question.java
+++ Question.java
@@ -1,22 +1,18 @@
 package quiz_console;
 
+import java.util.Arrays;
+
 public class Question {
     
     private int id;
     private String question;
-    private String option1;
-    private String option2;
-    private String option3;
-    private String option4;
+    private String[] options;
     private String answer;
     
-    public Question(int id, String question, String option1, String option2, String option3, String option4, String answer) {
+    public Question(int id, String question, String[] options, String answer) {
         this.id = id;
         this.question = question;
-        this.option1 = option1;
-        this.option2 = option2;
-        this.option3 = option3;
-        this.option4 = option4;
+        this.options = options;
         this.answer = answer;
     }
     public int getId() {
@@ -31,29 +27,11 @@ public class Question {
     public void setQuestion(String question) {
         this.question = question;
     }
-    public String getOption1() {
-        return option1;
+    public String[] getOptions() {
+        return options;
     }
-    public void setOption1(String option1) {
-        this.option1 = option1;
-    }
-    public String getOption2() {
-        return option2;
-    }
-    public void setOption2(String option2) {
-        this.option2 = option2;
-    }
-    public String getOption3() {
-        return option3;
-    }
-    public void setOption3(String option3) {
-        this.option3 = option3;
-    }
-    public String getOption4() {
-        return option4;
-    }
-    public void setOption4(String option4) {
-        this.option4 = option4;
+    public void setOptions(String[] options) {
+        this.options = options;
     }
     public String getAnswer() {
         return answer;
@@ -62,7 +40,9 @@ public class Question {
         this.answer = answer;
     }
     public String toString() {
-        return "Question [id=" + id + ", question=" + question + ", option1=" + option1 + ", option2=" + option2 + ", option3=" + option3 + ", option4=" + option4 + ", answer=" + answer + "]";
+        return "Question [id=" + id + ", question=" + question + ", options=" + Arrays.toString(options) + ", answer="
+                + answer + "]";
     }
+    
 
 }
--- QuestionService.java
+++ QuestionService.java
@@ -9,26 +9,27 @@ public class QuestionService {
 
     public QuestionService() {
         questions = new Question[5];
-        questions[0] = new Question(1, "What programming language is this written in?", "Java", "C++", "Python", "Rust", "Java");
-        questions[1] = new Question(2, "What programming language is this written in?", "Java", "C++", "Python", "Rust", "Java");
-        questions[2] = new Question(3, "What programming language is this written in?", "Java", "C++", "Python", "Rust", "Java");
-        questions[3] = new Question(4, "What programming language is this written in?", "Java", "C++", "Python", "Rust", "Java");
-        questions[4] = new Question(5, "What programming language is this written in?", "Java", "C++", "Python", "Rust", "Java");
+        questions[0] = new Question(1, "What programming language is this written in?", new String[] {"Java", "C++", "Python", "Rust"}, "Java");
+        questions[1] = new Question(2, "Which fast food restaurant?", new String[] {"Innout", "Five Guys", "Mcdonalds", "Burger King"}, "Innout");
+        questions[2] = new Question(3, "What programming language is this written in?", new String[] {"Java", "C++", "Python", "Rust"}, "Java");
+        questions[3] = new Question(4, "What programming language is this written in?", new String[] {"Java", "C++", "Python", "Rust"}, "Java");
+        questions[4] = new Question(5, "What programming language is this written in?", new String[] {"Java", "C++", "Python", "Rust"}, "Java");
         selections = new String[5];
     }
     public void playQuiz() {
         int i = 0;
+        Scanner scanner = new Scanner(System.in);
         for (Question q : questions) {
             System.out.println("Question " + q.getId());
             System.out.println(q.getQuestion());
-            System.out.println(q.getOption1());
-            System.out.println(q.getOption2());
-            System.out.println(q.getOption3());
-            System.out.println(q.getOption4());
-            Scanner scanner = new Scanner(System.in);
+            System.out.println(q.getOptions()[0]);
+            System.out.println(q.getOptions()[1]);
+            System.out.println(q.getOptions()[2]);
+            System.out.println(q.getOptions()[3]);
             selections[i] = scanner.nextLine();
             i ++;
         }
+        scanner.close();
         System.out.println("Your selections are:");
         for (String s : selections) {
             System.out.println(s);
