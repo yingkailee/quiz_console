00a13de5f12df6dddaf8d3fc02663be7b0a817e5
yingkailee <yingkai@Mac.attlocal.net>
2025-12-07 14:44:55 -0600

    finished 5 question quiz

--- Main.java
+++ Main.java
@@ -4,7 +4,8 @@ public class Main {
     
     public static void main(String[] args) {
         QuestionService service = new QuestionService();
-        service.displayQuestions();
+        service.playQuiz();
+        service.printScore();
     }
 
 }
--- QuestionService.java
+++ QuestionService.java
@@ -1,8 +1,11 @@
 package quiz_console;
 
+import java.util.Scanner;
+
 public class QuestionService {
 
     private Question[] questions;
+    private String[] selections;
 
     public QuestionService() {
         questions = new Question[5];
@@ -11,11 +14,34 @@ public class QuestionService {
         questions[2] = new Question(3, "What programming language is this written in?", "Java", "C++", "Python", "Rust", "Java");
         questions[3] = new Question(4, "What programming language is this written in?", "Java", "C++", "Python", "Rust", "Java");
         questions[4] = new Question(5, "What programming language is this written in?", "Java", "C++", "Python", "Rust", "Java");
+        selections = new String[5];
     }
-    public void displayQuestions() {
+    public void playQuiz() {
+        int i = 0;
         for (Question q : questions) {
-            System.out.println(q);
+            System.out.println("Question " + q.getId());
+            System.out.println(q.getQuestion());
+            System.out.println(q.getOption1());
+            System.out.println(q.getOption2());
+            System.out.println(q.getOption3());
+            System.out.println(q.getOption4());
+            Scanner scanner = new Scanner(System.in);
+            selections[i] = scanner.nextLine();
+            i ++;
         }
+        System.out.println("Your selections are:");
+        for (String s : selections) {
+            System.out.println(s);
+        }
+    }
+    public void printScore() {
+        int score = 0;
+        for (int i = 0; i < questions.length; i ++) {
+            if (questions[i].getAnswer().equalsIgnoreCase(selections[i])) {
+                score ++;
+            }
+        }
+        System.out.println("Your score is: " + score);
     }
 
 }
